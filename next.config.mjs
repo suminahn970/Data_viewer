0f34f1957c67fbefb201103a57ce280f85c5271e
suminahn <suminahn@suminahnui-MacBookAir.local>
2025-12-29 17:19:23 +0900

    Fix: Hardcode API URL to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // ⭐️ 아래 env 설정을 추가하여 백엔드 주소를 강제로 연결합니다.
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://data-viewer-zyxg.onrender.com',
+  },
 }
 
 export default nextConfig
